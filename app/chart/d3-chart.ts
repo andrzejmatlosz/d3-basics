b22b4278a7c3414fe4bee1a7a9bcfbcf0b4de6c6
Matłosz <ajms@gft.com>
2017-04-06 14:16:32 +0200

    solution from workshop

--- app/chart/d3-chart.ts
+++ app/chart/d3-chart.ts
@@ -19,11 +19,11 @@ export class D3Chart {
     paintLayer: d3.Selection<d3.BaseType, any, HTMLElement, any>;
     margin: Margin = { top: 20, right: 20, bottom: 30, left: 40 };
     points: Array<Point> = [ 
-        { x: 10, y: 20 },
-        { x: 15, y: 40 },
+        { x: -10, y: 20 },
+        { x: -15, y: 40 },
         { x: 21, y: 7 },
-        { x: 24, y: 36 },
-        { x: 36, y: 31 },
+        { x: 24, y: -36 },
+        { x: 36, y: -31 },
         { x: 45, y: 3 },
         { x: 50, y: 15 }
     ];
@@ -34,30 +34,78 @@ export class D3Chart {
 
     createVisualization() {
         this.svg = d3.select<Element, any>('.chart-svg');
-        // this.width = 
-        // this.height = 
-        // this.paintLayer = 
+        this.width = +this.svg.attr('width') - this.margin.left - this.margin.right;
+        this.height = +this.svg.attr('height') - this.margin.top - this.margin.bottom;
+        this.paintLayer = this.svg.append('g')
+            .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
     }
 
     addAxis() {
+        let minX = d3.min(this.points.map(point => point.x));
         let maxX = d3.max(this.points.map(point => point.x));
         let maxY = d3.max(this.points.map(point => point.y));
-        // this.xScale = 
-        // this.yScale = 
-        // draw axis
+        let minY = d3.min(this.points.map(point => point.y));
+
+        this.xScale = d3.scaleLinear()
+            .domain([minX, maxX])
+            .range([0, this.width]);
+        
+        this.yScale = d3.scaleLinear()
+            .domain([minY, maxY])
+            .range([this.height, 0]);
+
+        this.paintLayer
+            .append('g')
+            .attr('transform', `translate(0,${this.yScale(0)})`)
+            .call(d3.axisBottom(this.xScale));
+
+        this.paintLayer
+            .append('g')
+            .attr('transform', `translate(${this.xScale(0)},0)`)
+            .call(d3.axisLeft(this.yScale));
     }
 
     addLine() {
-        // let line: any = 
-        // this.paintLayer.append(
+        let line: any = d3.line()
+            .x((data: any) => this.xScale(data.x))
+            .y((data: any) => this.yScale(data.y));
+
+        this.paintLayer.append('path')
+            .data([this.points])
+            .attr('class', 'line')
+            .attr('d', line);
     }
 
     addCrosshair() {
         let paintLayer = this.paintLayer;
         let height = this.height;
+        let xMouse;
 
-        // let overlay = 
+        let overlay = this.paintLayer.append('rect')
+            .attr('class', 'overlay')
+            .attr('width', this.width)
+            .attr('height', this.height);
 
-        // overlay.on(
+        let lineAttr = (line) => {
+            line.attr('x1', xMouse)
+                .attr('x2', xMouse);
+        }
+
+        overlay.on('mouseover', function() {
+            xMouse = d3.mouse(this as any)[0];
+            paintLayer.append('line')
+                .attr('class', 'crosshair-line')
+                .attr('y1', 0)
+                .attr('y2', height)
+                .call(lineAttr);
+        })
+        .on('mousemove', function() {
+            xMouse = d3.mouse(this as any)[0];
+            paintLayer.select('.crosshair-line')
+                .call(lineAttr);
+        })
+        .on('mouseout', function() {
+            paintLayer.select('.crosshair-line').remove();
+        });
     }
 }
--- app/dashboard/d3-sample.ts
+++ app/dashboard/d3-sample.ts
@@ -4,7 +4,7 @@ import * as d3 from 'd3';
 @Injectable()
 export class D3Sample {
     svg: d3.Selection<Element, any, HTMLElement, any>;
-    data: Array<number> = [ 160, 40, 210 ];
+    data: Array<number> = [ 160, 40, 210, 360, 70 ];
 
     createVisualization() {
         this.svg = d3.select<Element, any>("svg");
@@ -12,52 +12,110 @@ export class D3Sample {
 
     setEqualSize() {
         // set size of all rects to 50x50 and fill to antiquewhite 
+        this.svg.selectAll('rect')
+            .attr('width', 50)
+            .attr('height', 50)
+            .style('fill', 'antiquewhite');
     }
 
     private setParams(rect) {
-        // set parameters of rect to 
-        // x: 10, 
-        // y: adjusted to index of rect
-        // width: set to value from domain
-        // height: 20
-        // fill: antiquewhite
+        rect.attr('x', 10)
+            .attr('height', 20)
+            .attr('width', (data, index) => { return data; })
+            .attr('y', (data, index) => 10 + index * 30);
     }
 
     setWidthAsData() {
-        // set width according to data
+        let bindedData = this.svg.selectAll('rect')
+            .data(this.data);
+
+        bindedData.call(this.setParams);
+
+        bindedData.enter()
+            .append('rect')
+            .call(this.setParams);
+
+        bindedData.exit()
+            .remove();
     }
 
     updateData() {
         this.data[1] = 300;
         this.data[3] = 280;
-        // update first and third rects
+
+        this.svg.selectAll('rect')
+            .data(this.data)
+            .transition()
+            .duration(1500)
+            .attr('width', data => data);
     }
 
     removeData() {
-        this.data.splice(1, 1);
+        //rect(__data__ = 160) 
+        //rect(__data__ = 40) 
+        //rect(__data__ = 210) 
+        //rect(__data__ = 360) 
+        //rect(__data__ = 70)
+        
+        //[ 160, 40, 210, 360, 70 ]
+
+        this.data.splice(1, 1); //[ 160, 210, 360, 70 ]
         this.data.splice(2, 1);
-        // remove first and third rects
-        // add animation for removing
+
+        //[ 160, 210, 70 ]
+        // rect(160) !rect rect(210) !rect! rect(70)
+        
+        let bindedData = this.svg.selectAll('rect')
+            .data(this.data as any, data => {
+                console.log(data);
+                let temp = (((data as number) + 100) as any);
+                console.log(temp);
+                return temp;
+            });
+
+        console.log('before exit');
+        bindedData
+            .exit().remove();
+        console.log('after exit');
+
+        bindedData.call(this.setParams);
     }
 
     setCircles() {
         // remove rects
+        this.svg.selectAll('rect')
+            .remove();
+
         // add circles
+        this.svg.selectAll('circle')
+            .data(this.data)
+            .enter()
+            .append('circle')
+            .attr('cx', (d, i) => 50 + i * 100)
+            .attr('cy', 100)
+            .attr('r', d => d / 10)
+            .style('fill', 'antiquewhite');
     }
 
     private interactions(element) {
         element.on('mouseover', function() {
-            // set stroke width of selected element to 4 
+            d3.select(this).style('stroke-width', 4);
         })
         .on('mouseout', function() {
-            // set stroke width of selected element to 2
+            d3.select(this).style('stroke-width', 2);
         })
         .on('click', function() {    
-            // set fill of selected element to yellow
+            d3.select(this).style('fill', 'yellow');
         });
     }
 
     addInteractions() {
+        this.svg.selectAll('rect')
+            .call(this.interactions);
+
+        this.svg.selectAll('circles')
+            .call(this.interactions);
+
         // add interactions into all rects and all circles
     }
 }
